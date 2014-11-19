ebcd37fa27b35b231e528b57d5a9baa4a6b20911
Johann <git@johann-hofmann.com>
2014-11-20 00:20:40 +0100

    Implement Block and Tag traits

--- src/lib.rs
+++ src/lib.rs
@@ -13,11 +13,26 @@ mod text;
 mod lexer;
 mod parser;
 
+pub trait Block {
+    fn initialize(&self, tag_name: &str, arguments: &str, tokens: Vec<Box<Renderable>>);
+    fn render(&self, context: &HashMap<String, String>) -> String;
+}
+
+pub trait Tag {
+    fn initialize(&self, tag_name: &str, arguments: &str, tokens: Vec<Box<Renderable>>);
+    fn render(&self, context: &HashMap<String, String>) -> String;
+}
+
+pub struct LiquidOptions<'a> {
+    blocks : Vec<Box<Block + 'a>>,
+    tags : Vec<Box<Tag + 'a>>
+}
+
 pub trait Renderable {
     fn render(&self, context: &HashMap<String, String>) -> String;
 }
 
-pub fn parse (text: &str) -> Template{
+pub fn parse<'a> (text: &str, options: LiquidOptions) -> Template<'a>{
     let tokens = lexer::tokenize(text.as_slice());
     let renderables = parser::parse(tokens);
     Template::new(renderables)
@@ -27,7 +42,7 @@ pub fn parse (text: &str) -> Template{
 fn test_liquid() {
     let mut data = HashMap::new();
     data.insert("hello".to_string(), "world".to_string());
-    let template = parse("wat\n{{hello}} test");
+    let template = parse("wat\n{{hello}} test", LiquidOptions{blocks:vec![], tags:vec![]});
     let output = template.render(&data);
     assert_eq!(output, "wat\nworld test".to_string());
 }
--- src/parser.rs
+++ src/parser.rs
@@ -9,14 +9,14 @@ use lexer::Element::{Output, Tag, Raw};
 pub fn parse<'a> (tokens: Vec<Element>) -> Vec<Box<Renderable + 'a>> {
     tokens.iter().map(|token| {
         match token {
-            &Output(ref tokens,_) => parse_token(tokens),
+            &Output(ref tokens,_) => parse_output(tokens),
             &Tag(_,_) => box Variable::new("tag") as Box<Renderable>,
             &Raw(ref x) => box Text::new(x.as_slice()) as Box<Renderable>
         }
     }).collect()
 }
 
-fn parse_token<'a> (tokens: &Vec<Token>) -> Box<Renderable + 'a> {
+fn parse_output<'a> (tokens: &Vec<Token>) -> Box<Renderable + 'a> {
     let ret = match tokens[0] {
         Identifier(ref x) => box Variable::new(x.as_slice()),
          _ => box Variable::new("output")
